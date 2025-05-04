dee7814a575ceb4b733a8fa4077104761296bccc
Przemek <mcsteamy24@gmail.com>
2025-05-04 18:57:14 +0200

    added RTK for movies

--- frontend/src/redux/api/apiSlice.jsx
+++ frontend/src/redux/api/apiSlice.jsx
@@ -1,7 +1,7 @@
 // import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { createApi } from '@reduxjs/toolkit/query/react'
 import { fetchBaseQuery } from '@reduxjs/toolkit/query'
-import { BASE_URL, USERS_URL, GENRE_URL } from '../constants'
+import { BASE_URL, USERS_URL, GENRE_URL, MOVIE_URL, UPLOAD_URL } from '../constants'
 
 export const apiSlice = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
@@ -68,8 +68,68 @@ export const apiSlice = createApi({
 				
 			})
 		}),
-		
+
+		getAllMovies: builder.query({
+			query:()=> `${MOVIE_URL}/all-movies`
+		}),
+
+		createMovie:builder.mutation({
+			query:(newMovie)=>({
+				url: `${MOVIE_URL}/create-movie`,
+				method:'POST',
+				body:newMovie
+			})
+		}),
+		updateMovie:builder.mutation({
+			query:({id,updatedMovie})=>({
+				url:`${MOVIE_URL}/update-movie/${id}`,
+				method:'PUT',
+				body:updatedMovie
+			})
+		}),
+		addMovieReview:builder.mutation({
+			query:({id,rating,comment})=>({
+				url:`${MOVIE_URL}/${id}/reviews`,
+				method:'POST',
+				body:{rating,id,comment}
+			})
+		}),
+		deleteComment:builder.mutation({
+			query:({movieId,reviewId})=>({
+				url:`${MOVIE_URL}/delete-comment`,
+				method:'DELETE',
+				body:{movieId,reviewId}
+			})
+		}),
+		deleteMovie:builder.mutation({
+			query:(id)=>({
+				url:`${MOVIE_URL}/delete-movie/${id}`,
+				method:'DELETE',
+				
+			})
+		}),
+		getSpecificMovie:builder.query({
+			query:(id)=>`${MOVIE_URL}/specific-movie/${id}`
+		}),
+
+		uploadImage:builder.mutation({
+			query:(formData)=>({
+				url:`${UPLOAD_URL}`,
+				method:'POST',
+				body:formData
+			})
+		}),
+		getNewMovies:builder.query({
+			query:()=>`${MOVIE_URL}/new-movies`
+		}),
+		getTopMovies:builder.query({
+			query:()=>`${MOVIE_URL}/top-movies`
+		}),
+		getRandomMovies:builder.query({
+			query:()=>`${MOVIE_URL}/random-movies`
+		})
+
 	}),
 })
 
-export const { useLoginMutation, useRegisterMutation,useLogoutMutation,useProfileMutation, useCreateGenreMutation,useUpdateGenreMutation,useDeleteGenreMutation,useFetchGenresQuery } = apiSlice
+export const { useLoginMutation, useRegisterMutation,useLogoutMutation,useProfileMutation, useCreateGenreMutation,useUpdateGenreMutation,useDeleteGenreMutation,useFetchGenresQuery,useGetAllMoviesQuery,useUpdateMovieMutation,useAddMovieReviewMutation,useDeleteCommentMutation,useDeleteMovieMutation,useGetSpecificMovieQuery,useUploadImageMutation,useGetNewMoviesQuery,useGetTopMoviesQuery,useGetRandomMoviesQuery } = apiSlice
